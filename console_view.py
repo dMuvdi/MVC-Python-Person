dc1b988150c0eb924126b0b04b5850a7f12c242b
eiker07 <eduarjose430@gmail.com>
2022-08-24 18:25:19 -0500

    congruencias

--- console_view.py
+++ console_view.py
@@ -6,12 +6,12 @@ def person_founded():
     print("Please type a different id")
 
 def empty_input():
-    print("Please type something do not left the input empty")
+    print("Please type something do not left the input empty.")
 
 
 
 def inserted_on_web():
-    print("inserted on web")
+    print("inserted on web.")
 
 
 def person_update(id_per, name, last_name):
@@ -19,7 +19,7 @@ def person_update(id_per, name, last_name):
     print("\nid: ", id_per)
     print("name: ", name)
     print("last name: ", last_name)
-    print("\n\nIt is important to know that the id is uneditable you can only edit the name and last name")
+    print("\n\nIt is important to know that the id is uneditable you can only edit the name and last name.")
 
 
 def person_delete(id_per, name, last_name):
@@ -30,12 +30,12 @@ def person_delete(id_per, name, last_name):
 
 
 def deleted_on_web():
-    print("deleted on web")
+    print("deleted on web.")
 
 
 def wrong_answer():
-    print("Please type the answers from above (y / n), you typed a diferent one")
+    print("Please type the answers from above (y / n), you typed a diferent one.")
 
 
 def updated_on_web():
-    print("updated on web")
+    print("updated on web.")
